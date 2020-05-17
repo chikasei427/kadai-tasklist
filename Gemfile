d6f0f454dc52363ad393fad1f45cad659d0ca202
EC2 Default User <ec2-user@ip-172-31-31-1.ec2.internal>
2020-05-17 07:17:15 +0000

    for heroku

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.3'
 
+gem 'pry-rails'
 gem 'kaminari'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
@@ -26,7 +27,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -51,3 +52,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+group :production do
+  gem 'pg', '>= 0.18', '< 2.0'
+end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,32 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:destroy]
+    before_action :require_user_logged_in, only: [:new, :edit, :show]
     def index
-      @tasks = Task.all
+      if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      else
+        redirect_to login_url
+      end
     end
     
     def show
     end
-    
+
     def new
       @task = Task.new
     end
-    
+  
     def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
       if @task.save
-        flash[:success] = 'Task が正常に投稿されました'
-        redirect_to @task
+        flash[:success] = 'タスクを投稿しました。'
+        redirect_to root_url
       else
-        flash.now[:danger] = 'Task が投稿されませんでした'
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        flash.now[:danger] = 'タスクの投稿に失敗しました。'
+        session[:user_id] = @task.user.id
         render :new
       end
     end
@@ -37,9 +46,8 @@ class TasksController < ApplicationController
     
     def destroy
       @task.destroy
-      
-        flash[:success] = 'Task は正常に削除されました'
-        redirect_to tasks_url
+      flash[:success] = 'メッセージを削除しました。'
+      redirect_back(fallback_location: root_path)
     end
     
     private
@@ -51,5 +59,11 @@ class TasksController < ApplicationController
     def task_params
         params.require(:task).permit(:content, :status)
     end
-    
+
+    def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+        redirect_to root_url
+      end
+    end
 end
